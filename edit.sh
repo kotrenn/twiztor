e7d6b78a1319e0534506ad781bf9269e0faf60cb
Ramona Seay <kotrenn@gmail.com>
2017-06-17 22:01:03 -0700

    PUZ: Added map.txt, world.txt, *.sh to edit.sh

--- edit.sh
+++ edit.sh
@@ -1,1 +1,1 @@
-emacs -nw src/cpp/*.h src/cpp/*.cpp src/java/*.java src/js/*.html src/js/*.js shaders/* puzzles/*.txt *.py doxygen.txt makefile *.bat
+emacs -nw src/cpp/*.h src/cpp/*.cpp src/java/*.java src/js/*.html src/js/*.js shaders/* puzzles/*.txt *.py doxygen.txt makefile *.bat *.sh map.txt world.txt
